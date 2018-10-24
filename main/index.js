2b75b0b39546b6868afc1e9790e7fb3ad196586f
rodriguezcesar1 <rodriguezcesar1214@gmail.com>
2018-10-23 22:22:29 -0500

    Initial commit

---
+++ main/index.js
@@ -0,0 +1,15 @@
+var codevalue = "function myScript(){return 100;}\n";
+var codevalue1 = `for (var line=1; line<60; line++) {\n
+  for(var i=1;i<29;i++) {\n
+    var s = (Math.floor((Math.random()*2)%2)) ? '/' : '/';\n
+    document.write(s);\n
+  }\n
+  document.writeln('<br>');\n
+}\n`
+var codevalue2 = `<html>
+  <head>
+  </head>
+<body>
+  <p> this is a text editor</p>
+  </body>
+  </html>`
